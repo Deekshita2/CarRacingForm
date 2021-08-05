959cbe4d9e23b18c3037e731b934838d0e31f6db
Deekshita2 <78123838+Deekshita2@users.noreply.github.com>
2021-08-04 21:04:43 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CarRacingForm
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,24 @@
+var  database, player, game, form, playerCount
+var gameState = 0;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+
+    game = new Game();
+    game.getState();
+    game.start();
+   
+}
+
+function draw(){
+    background("white");
+    
+    drawSprites();
+}
+
+
+
+
+
+
